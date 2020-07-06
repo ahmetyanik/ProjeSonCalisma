e482bfa8920b1ae9a2590c3f63970e61cad2f1c7
ahmetyanik <ahmetyank4242@gmail.com>
2020-07-06 19:39:20 +0200

    v2

--- Dünya.java
+++ Dünya.java
@@ -7,6 +7,11 @@ public class Dünya {
 		System.out.println("1");		
 		System.out.println("master calismasi basladi");
 		System.out.println("remotta da düzenleme yapildi");
+		System.out.println("2");
+		System.out.println("ikinci gün calismalari");
+		
+		
+		
 		
 		
 		
