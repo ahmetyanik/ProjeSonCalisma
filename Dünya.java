91c029b2a6d328d97bd447a78522e4fe3bb88664
ahmetyanik <ahmetyank4242@gmail.com>
2020-07-06 19:25:55 +0200

    1

---
+++ Dünya.java
@@ -0,0 +1,15 @@
+package dunya;
+
+public class Dünya {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("1");		
+		System.out.println("master calismasi basladi");
+		
+		
+		
+	}
+	
+
+}
